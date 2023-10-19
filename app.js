f4c5db3bba41119f66a107c03e252d8e824e62ff
mckiernantim <mckiernantim@gmail.com>
2023-10-18 20:15:48 -0400

    port fixe

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 
-const PUERTO = process.env.port || 8080;
+const PUERTO = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
     res.send("render is working!@#@!")
